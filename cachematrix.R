96419b324cc009007b5415a61e91d4dafbd9a559
Juancho322 <72739714+Juancho322@users.noreply.github.com>
2020-10-18 07:48:58 -0500

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,23 @@
-## Beneath are a pair of functions that are used to create a special object that 
+##                    ****Caching the Inverse of a Matrix****
+## Description:
+## Matrix inversion is usually a costly computation and there may be some benefit
+## to "caching the inverse of a matrix" rather than compute it repeatedly.
+
+## In the present work, two functions are used to create a special object that 
 ## stores a matrix and caches its inverse.
 
+## makeCacheMatrix
 ## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
         inv <- NULL
-        set <- function(y) {
+        set <- function(y) {                               ## set the value of the matrix 
                 x <<- y
                 inv <<- NULL
         }
-        get <- function() x
-        setInverse <- function(inverse) inv <<- inverse
-        getInverse <- function() inv
+        get <- function() x                                ## get the value of the matrix
+        setInverse <- function(inverse) inv <<- inverse    ## set the value of the inverse
+        getInverse <- function() inv                       ## get the value of the inverse
         list(set = set,
              get = get,
              setInverse = setInverse,
@@ -19,15 +25,16 @@ makeCacheMatrix <- function(x = matrix()) {
 }
 
 
-## This function computes the inverse of the special "matrix" created by 
-## makeCacheMatrix above. If the inverse has already been calculated, 
-## then it should retrieve the inverse from the cache.
+## cacheSolve
+## This function computes the inverse of the special "matrix" returned by 
+## makeCacheMatrix above. If the inverse has already been calculated (and the 
+## matrix has not changed), then it should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-        inv <- x$getInverse()
+                                                         ## Return a matrix that is the
+        inv <- x$getInverse()                            ## inverse of "x"
         if (!is.null(inv)) {
-                message("getting cached data")
+                message("getting cached data")           ## The operation is displayed
                 return(inv)
         }
         mat <- x$get()
